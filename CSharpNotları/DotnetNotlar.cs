78053dd2423f4365e1fe09a8504baf902e2dc77b
Kaan Akcan <kaan.akcan@performans.com>
2021-12-22 17:52:11 +0300

    update

--- CSharpNotları/DotnetNotlar.cs
+++ CSharpNotları/DotnetNotlar.cs
@@ -28,6 +28,24 @@
 
 
         }
+        List<FileInfo> DosyaYazdir(string path)
+        {
+            List<FileInfo> fileInfos = new();
+            DirectoryInfo directoryInfo = new(path);
+            DirectoryInfo[] directoryInfos = directoryInfo.GetDirectories();
+            if(directoryInfos.Any())
+            {
+                foreach(DirectoryInfo directory in directoryInfos)
+                {
+                    fileInfos.AddRange(DosyaYazdir(directory.FullName));
+                }
+            }
+            else
+            {
+                fileInfos.AddRange(directoryInfo.GetFiles());
+            }
+            return fileInfos;
+        }
     }
 
 }
