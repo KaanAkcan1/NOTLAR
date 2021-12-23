85fd4a15d484cd6bd40ac7e2eb0689af23d1c71e
Kaan Akcan <kaan.akcan@performans.com>
2021-12-23 14:39:32 +0300

    up

---
+++ CSharpNotları/OOP.cs
@@ -0,0 +1,26 @@
+﻿namespace CSharpNotları
+{
+    public class OOP
+    {
+        #region Propertyİmzaları()
+
+        int yasi;
+        string b;
+        #region FullProperty
+        //[erişimbelirleyici][içeriktipi][propertyadı]
+        //{
+        //    get { }/* =>Property'den veri istendiğinde tetiklenir.*/
+        //    set{ } //=> Propertyİmzaları'e veri gönderildiğinde tetiklenir.
+        //    //    Gönderilen veriyi value keywordü ile yakalar.
+        //    // Set bloğu tanımlanmazsa readonly, get bloğu tanımlanmazsa writeonly olur
+        //}
+        public int Yasi
+        {
+            get { return yasi; }
+            set { yasi = value; }
+        }
+
+        #endregion
+        #endregion
+    }
+}
