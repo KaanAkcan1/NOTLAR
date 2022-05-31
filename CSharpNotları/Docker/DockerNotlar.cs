b3c341ae37784269b4c8b9e46373ab0bc3d6bbb3
Kaan Akcan <kaan.akcan@performans.com>
2022-05-31 14:14:02 +0300

    update

---
+++ CSharpNotları/Docker/DockerNotlar.cs
@@ -0,0 +1,59 @@
+﻿namespace CSharpNotları.Docker
+{
+    public class DockerNotlar
+    {
+        #region İmage indirme yükleme Çalıştırma
+        //docker pull mongo
+        //=> mongo image indirir
+
+        //docker run mongo
+        //=> mongo image çalıştırır yoksa indirir
+
+        //docker run it --name bash_ubuntu ubuntu
+        //=> ubuntu image'i bash_ubuntu ismiyle çalıştırır
+
+        //docker run it ubuntu
+        //=>ubuntuyu çalıştırır ve kapanmasını engeller. Normalde çalışır vaziyette kalmaz.
+
+        //docker run ubuntu sleep 3
+        //=> 3 saniyelik çalışır vaziyette kalır
+
+        //docker start bash_ubuntu
+        //=> bash ubuntu ismindeki containerı çalıştırır. Container Id de kullanılabilir.
+
+        //docker stop bash_ubuntu
+        //=> containeri durdurur
+
+        //docker rm 03 bf 22
+        //=> boşluk bırakılarak yazılabilen çoklu ve ya tek yaılan container geçmişini siler
+
+        //docker container rm $(docker container ls -aq)
+        //=>bütün hepsini siler
+        #endregion
+        #region Çalışan veya çalışmış container listeleme
+        //ll
+
+        //ls
+
+        //docker ps
+        //=> çalışan containerlerı gösterir
+
+        //docker ps -a
+        //=> Geçmişe dair çalışanları gösterir
+
+        //docker container ls
+        //=> aktif olan containerlar
+
+        //docker container ls -a
+        //=> aktif olan yada olmayan tüm containerlerı gösterir
+
+        //docker images
+        //=> var olan imageleri gösterir
+
+        //exit
+        //=> kapama
+
+
+        #endregion
+    }
+}
